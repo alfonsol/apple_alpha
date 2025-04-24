888260d3067037c868e4a1b79c19b251560393fe
Alfonso <alfonsol@velopez.com>
2025-04-24 13:29:46 -0500

    fixing merge conflict

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
+	var redisClient *redis.Client
+
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+	ctx := context.Background()
+	_, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to Redis: %v", err))
+	}
+	fmt.Println("Connected to Redis")
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Internal Server Error"))
